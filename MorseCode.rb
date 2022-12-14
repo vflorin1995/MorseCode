bbeb14a90d67324909464f81d4cb0b7cc7ee90ce
vflorin1995 <vflorin1995@gmail.com>
2022-12-14 16:18:23 +0200

    Add methods

---
+++ MorseCode.rb
@@ -0,0 +1,59 @@
+MorseCodeLetters = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '-----' => '0',
+  '/' => ' '
+}
+
+def morse_letter(n)
+  MorseCodeLetters[n]
+end
+
+def morse_word(str)
+  result = ''
+  str.split().each{ |n| result.concat(MorseCodeLetters[n]) }
+  result
+end
+
+def morse_phrase (str)
+  result = ''
+  str1 = str.split('   ').each do |n| 
+    result.concat(morse_word(n))
+    result.concat(' ')
+  end
+  result = result.strip
+  result
+end
